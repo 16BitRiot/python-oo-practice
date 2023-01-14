ece3845378dfb4f06591d223318efc9196cae3fd
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2023-01-13 22:05:55 -0500

    commit

--- wordfinder.py
+++ wordfinder.py
@@ -2,20 +2,26 @@
 import os
 import random
 
+
 class WordFinder:
     ...
-    def __init__(self, path): 
+
+    def __init__(self, path):
         self.file_path = os.path.join(os.path.dirname(__file__), path)
-        """capture the files pathway in a variable"""
-        self.words =[]
+        """capture the files pathway in a new variable"""
         with open(self.file_path, "r") as x:
             """open the file as read only, and store its contents in x"""
             self.lines_count = len(x.readlines())
-            self.words = x.readlines()
-            print(self.lines_count)
 
     def random(self):
-        rando =  self.words[random.randrange(0, self.lines_count -1)].strip()
-        return rando
+        with open(self.file_path, "r") as y:
+            """open the file as read only, and store its contents in y"""
+            self.words = y.readlines
+            """capture all lines in the file into a new variable"""
+            return self.words()[random.randrange(0, self.lines_count - 1)]
+            """returns a random selection by generating a number between one and the highest number in the doc, and uses that number as an index number"""
 
-webster = WordFinder("/Users/philfryer/Desktop/School Spring/Unit21.4pythonoop/python-oo-practice/words.txt")
+
+webster = WordFinder(
+    "/Users/philfryer/Desktop/School Spring/Unit21.4pythonoop/python-oo-practice/words.txt")
+print(webster.random())
