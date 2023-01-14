0cb092bcf7adb57cfa39b0d07c0f13de1bb245f2
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2023-01-13 20:35:12 -0500

    commit

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,21 @@
 """Word Finder: finds random words from a dictionary."""
-
+import os
+import random
 
 class WordFinder:
     ...
+    def __init__(self, path): 
+        self.file_path = os.path.join(os.path.dirname(__file__), path)
+        """capture the files pathway in a variable"""
+        self.words =[]
+        with open(self.file_path, "r") as x:
+            """open the file as read only, and store its contents in x"""
+            self.lines_count = len(x.readlines())
+            self.words = x.readlines()
+            print(self.lines_count)
+
+    def random(self):
+        rando =  self.words[random.randrange(0, self.lines_count -1)].strip()
+        return rando
+
+webster = WordFinder("/Users/philfryer/Desktop/School Spring/Unit21.4pythonoop/python-oo-practice/words.txt")
